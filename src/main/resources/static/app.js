4959e00e29de05ce8e3cc93da8064da3aa377b6a
crustedBeans <isaac.toasted@gmail.com>
2025-05-10 14:26:01 -0700

    Revert "ssl/tls implementation"
    This reverts commit 83496f359923ce03ffbf35e919a73c3aed42f361.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,5 +1,5 @@
 const stompClient = new StompJs.Client({
-  brokerURL: `wss://localhost:8080/gs-guide-websocket`,
+  brokerURL: `ws://${window.location.host}/gs-guide-websocket`,
 });
 
 stompClient.onConnect = (frame) => {
